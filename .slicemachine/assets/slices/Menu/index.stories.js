3501e54f8eb2b917019044b04240aac96a47e483
Dmsooja <dev@sooja.fr>
2022-11-10 18:24:48 +0100

    Create Menu ct and menu item slice with dropdown variation

---
+++ .slicemachine/assets/slices/Menu/index.stories.js
@@ -0,0 +1,12 @@
+import MyComponent from '../../../../slices/Menu';
+
+export default {
+  title: 'slices/Menu'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"company_name":"bound","logo":{"dimensions":{"width":900,"height":32},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1560457079-9a6532ccb118"},"label":[{"type":"paragraph","text":"Sint sint ipsum incididunt adipisicing commodo aliquip eu anim incididunt enim laboris aliqua laborum aliqua amet.","spans":[]}],"link":{"link_type":"Web","url":"https://slicemachine.dev"}},"slice_type":"menu","id":"_Default"}} />
+_Default.storyName = ''
+
+export const _Dropdown = () => <MyComponent slice={{"variation":"dropdown","version":"sktwi1xtmkfgx8626","items":[{"second_level_label":[{"type":"paragraph","text":"Tempor dolor aliquip do. Ullamco voluptate in mollit duis in aliqua id excepteur dolore irure qui ea ad.","spans":[]}],"second_level_link":{"link_type":"Web","url":"https://prismic.io"}}],"primary":{"company_name":"quick","logo":{"dimensions":{"width":900,"height":32},"alt":null,"copyright":null,"url":"https://images.unsplash.com/photo-1596195689404-24d8a8d1c6ea"},"dropdown_label":[{"type":"paragraph","text":"Consectetur adipisicing reprehenderit fugiat. Eiusmod non ex do aute consectetur. Pariatur irure enim et tempor laboris et laborum esse ea reprehenderit exercitation occaecat culpa mollit ipsum.","spans":[]}]},"slice_type":"menu","id":"_Dropdown"}} />
+_Dropdown.storyName = ''
