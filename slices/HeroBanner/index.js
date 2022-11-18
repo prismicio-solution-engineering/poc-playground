e5d8174a1a42074d728ba4e618bcc6469f8cb034
Dmsooja <dev@sooja.fr>
2022-11-18 18:10:47 +0100

    Fix margins and svg layer

--- slices/HeroBanner/index.js
+++ slices/HeroBanner/index.js
@@ -12,6 +12,15 @@ const HeroBanner = ({ slice }) => (
       <div className="relative mb-6 overflow-hidden bg-white">
         <div className="mx-auto max-w-7xl">
           <div className="relative z-10 bg-white pb-8 sm:pb-16 md:pb-20 lg:w-full lg:max-w-2xl lg:pb-28 xl:pb-32">
+          <svg
+            className="absolute inset-y-0 right-0 hidden h-full w-48 translate-x-1/2 transform text-white lg:block"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
             <div className="relative px-4 pt-6 sm:px-6 lg:px-8" />
             <main className="mx-auto mt-10 max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
               <div className="sm:text-center lg:text-left">
@@ -60,7 +69,7 @@ const HeroBanner = ({ slice }) => (
       </div>
     )}
     {slice.variation == "withSignUpAndMedia" && (
-      <main className="mx-auto mt-16 max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
+      <main className="mx-auto my-16 max-w-7xl px-4 sm:my-24 sm:px-6 lg:my-32">
         <div className="lg:grid lg:grid-cols-12 lg:gap-8">
           <div className="sm:text-center md:mx-auto md:max-w-2xl lg:col-span-6 lg:text-left">
             <div className="mt-1 block text-4xl font-bold tracking-tight sm:text-5xl xl:text-6xl">
