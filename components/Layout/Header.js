0ff79292933b49a06b9121292b5f6cacb1efb0d5
Dmsooja <dev@sooja.fr>
2022-12-02 22:40:44 +0100

    replace link with PrismicLink

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -19,14 +19,14 @@ export const Header = ({ menu, currentLocale, locales, alternatesUrls }) => (
       <div className="mx-auto px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
-            <a href="/">
+            <PrismicLink href="/">
               <span className="sr-only">{menu?.data?.company_name}</span>
               <img
                 className="h-8 w-auto sm:h-10"
                 src={menu?.data?.logo.url}
                 alt={menu?.data?.logo.alt}
               />
-            </a>
+            </PrismicLink>
           </div>
           <div className="-my-2 -mr-2 md:hidden">
             <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
