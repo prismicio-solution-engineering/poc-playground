8a52ea44a2de9ee7185b68d130478ebecf196dea
Dmsooja <dev@sooja.fr>
2022-12-05 19:14:29 +0100

    Change z-index of mobile menu

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -127,7 +127,7 @@ export const Header = ({ menu, currentLocale, locales, alternatesUrls }) => (
       >
         <Popover.Panel
           focus
-          className="absolute inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
+          className="absolute z-20 inset-x-0 top-0 origin-top-right transform p-2 transition md:hidden"
         >
           <div className="divide-y-2 divide-gray-50 rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
             <div className="px-5 pt-5 pb-6">
